9cde5177dd267c2a0934332dd2e85dacee28066a
rfreitasbatista <rfreitasbatista@gmail.com>
2020-08-16 18:25:31 -0300

    Updates

---
+++ precache-manifest.2358b96d448f5b2b5a342c89a2e47d8f.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78cf510d47fa4d73522e4b1f8b475000",
+    "url": "/index.html"
+  },
+  {
+    "revision": "854a1299f78a6e436661",
+    "url": "/static/css/2.ae27d00f.chunk.css"
+  },
+  {
+    "revision": "b92ca46763ddabfbb9ea",
+    "url": "/static/css/main.8e04b27b.chunk.css"
+  },
+  {
+    "revision": "854a1299f78a6e436661",
+    "url": "/static/js/2.7df07417.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.7df07417.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b92ca46763ddabfbb9ea",
+    "url": "/static/js/main.9439f683.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "3d950341f0114a0c9f240e83e9039a65",
+    "url": "/static/media/RQMColored.3d950341.png"
+  },
+  {
+    "revision": "a333c6febe481b07bf3eed9790d15c9b",
+    "url": "/static/media/covidFollowerBW.a333c6fe.jpeg"
+  },
+  {
+    "revision": "a15752c664ff8d2b0c67209b7eaf24b7",
+    "url": "/static/media/covidFollowerColor.a15752c6.jpeg"
+  },
+  {
+    "revision": "1fc079ced7f2970e37c7451d17773fd5",
+    "url": "/static/media/logo.1fc079ce.png"
+  },
+  {
+    "revision": "60e010b2486a0d4e08856ea3f5f45aef",
+    "url": "/static/media/rbFlixColor.60e010b2.jpg"
+  },
+  {
+    "revision": "f89ad78a1745742d78ef91fc1f11314b",
+    "url": "/static/media/solDivinoBW.f89ad78a.jpeg"
+  },
+  {
+    "revision": "0accafcd08d4a673e4957b627db30189",
+    "url": "/static/media/solDivinoColor.0accafcd.jpeg"
+  }
+]);
