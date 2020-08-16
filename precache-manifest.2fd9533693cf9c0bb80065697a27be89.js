da6ca0cef47feb9168f297dc44f01ef13d01752a
rfreitasbatista <rfreitasbatista@gmail.com>
2020-08-16 18:59:10 -0300

    Updates

---
+++ precache-manifest.2fd9533693cf9c0bb80065697a27be89.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f87e52c0689167726889091082578a67",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d9c40cf1ca8782bdea59",
+    "url": "/static/css/2.ae27d00f.chunk.css"
+  },
+  {
+    "revision": "e27e7696e2cba314295e",
+    "url": "/static/css/main.8e04b27b.chunk.css"
+  },
+  {
+    "revision": "d9c40cf1ca8782bdea59",
+    "url": "/static/js/2.ad03fbe3.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.ad03fbe3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e27e7696e2cba314295e",
+    "url": "/static/js/main.d0aa2635.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "3d950341f0114a0c9f240e83e9039a65",
+    "url": "/static/media/RQMColored.3d950341.png"
+  },
+  {
+    "revision": "a333c6febe481b07bf3eed9790d15c9b",
+    "url": "/static/media/covidFollowerBW.a333c6fe.jpeg"
+  },
+  {
+    "revision": "a15752c664ff8d2b0c67209b7eaf24b7",
+    "url": "/static/media/covidFollowerColor.a15752c6.jpeg"
+  },
+  {
+    "revision": "1fc079ced7f2970e37c7451d17773fd5",
+    "url": "/static/media/logo.1fc079ce.png"
+  },
+  {
+    "revision": "60e010b2486a0d4e08856ea3f5f45aef",
+    "url": "/static/media/rbFlixColor.60e010b2.jpg"
+  },
+  {
+    "revision": "f89ad78a1745742d78ef91fc1f11314b",
+    "url": "/static/media/solDivinoBW.f89ad78a.jpeg"
+  },
+  {
+    "revision": "0accafcd08d4a673e4957b627db30189",
+    "url": "/static/media/solDivinoColor.0accafcd.jpeg"
+  }
+]);
--- src/reducers/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux';
-import UsuarioReducer from './UsuarioReducer';
-
-export default combineReducers({
-  usuario: UsuarioReducer
-})
